efcd1a8c941d513bea1c47a201b704407f7561fe
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-03-29 21:43:32 -0300

    create SecurityFilter and updated UserRepository

--- src/main/java/com/example/loginauthapi/repositories/UserRepository.java
+++ src/main/java/com/example/loginauthapi/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.loginauthapi.repositories;
 import com.example.loginauthapi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
